0d51ccb77ff23671884267e84b71cd706cf7af8c
zhaoyu <444104595@qq.com>
2019-06-13 16:38:49 +0800

    初版提交，这是后端的代码。

--- apiHome-admin/src/main/java/io/renren/ApiHomeApplication.java
+++ apiHome-admin/src/main/java/io/renren/ApiHomeApplication.java
@@ -13,10 +13,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-public class RenrenApplication {
+public class ApiHomeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RenrenApplication.class, args);
+		SpringApplication.run(ApiHomeApplication.class, args);
 	}
 
 }
